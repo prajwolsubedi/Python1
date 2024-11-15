26fbe2d5373d8f800f7b750721d8fb13e2d4894b
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-11-15 15:52:45 +0545

    first commit

---
+++ input.py
@@ -0,0 +1,22 @@
+# input() -> A function that takes input from the user and returns it as a string.
+#general > code completion
+#plugin github copilot
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# age = age - 1
+# print(f"Hello, {name}! You are {age} years old.")
+
+#Area of a rectangle
+# length = float(input("Enter the length of the rectangle: "))
+# breadth = float(input("Enter the breadth of the rectangle: "))
+# area = length * breadth;
+# print(f"The area of the rectangle is {area} square units.")
+
+# item,price,quantity
+item = input("Enter the item you want to buy?: ")
+price = float(input("Enter the price of the item?: "))
+quantity = int(input("Enter how many items you want to add to the cart?: "))
+
+print(f"The total price is {price * quantity}")
+
+
